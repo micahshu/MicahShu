e90248f94db68d247a6ba319c961fbd125afa3c2
Micah Shumaker <micah.shumaker@gmail.com>
2024-01-01 19:18:18 -0700

    Started work on nav bar

--- portfolio/src/NavBar/NavBar.js
+++ portfolio/src/NavBar/NavBar.js
@@ -1,18 +1,41 @@
-import "./NavBar.css"
+import React, { useState } from 'react';
+import './NavBar.css';
 
+// Create a separate Menu component
+const Menu = ({ onClose }) => (
+    <>
+    {/* Add your menu items or components here */}
+    <div className="gridNavOne font" onClick={onClose}>Micah Shumaker</div>
+    <div className="gridNavTwo font" onClick={onClose}>Home</div>
+    <div className="gridNavThree font" onClick={onClose}>Projects</div>
+    <div className="gridNavFour font" onClick={onClose}>Blog</div>
+    <div className="gridNavFive font" onClick={onClose}>Contact</div>
+    </>
+  
+);
 
 function NavBar() {
-  
-    return (
-      <div className='NavBar'>
-        <div className="gridHome grid"> <p className="rotate">:)</p></div>
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
-        <div className="gridTitle grid">Micah Shumaker</div>
-        <div className="gridNavOne grid">Projects</div>
-        <div className="gridNavTwo grid">Blog</div>
-        <div className="gridNavThree grid">Contact</div>
-      </div>
-    );
-  }
-  
-  export default NavBar;
+  const handleMenuClick = () => {
+    // Toggle the menu state
+    setMenuOpen(!isMenuOpen);
+  };
+
+  const handleMenuItemClick = () => {
+    // Your menu item click logic goes here
+    console.log('Menu item clicked!');
+    // Close the menu after handling the click event
+  };
+
+  return (
+    <div className='NavBar'>
+      <div onClick={handleMenuClick} className="gridMenu grid"> <p className="rotate">:)</p></div>
+      
+      {/* Conditionally render the menu based on the state */}
+      {isMenuOpen && <Menu onClose={handleMenuItemClick} />}
+    </div>
+  );
+}
+
+export default NavBar;
