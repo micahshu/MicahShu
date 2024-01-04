bc62aa6847257a24f1bce0e18db07aba59b9ad85
Micah Shumaker <micah.shumaker@gmail.com>
2024-01-04 01:07:54 -0700

    Complete overhaul. Next.js + Tailwind

---
+++ portfolio-next/src/app/ThemeToggle/Homepage/Homepage.js
@@ -0,0 +1,29 @@
+
+
+export default function HomePage() {
+
+    return (
+        <div className="">
+        <div className="snap-y snap-mandatory overflow-y-scroll h-screen">
+
+            <section className="h-screen flex bg-black snap-start">
+                <h1>First Page</h1>
+            </section>
+
+            <section className="h-screen flex bg-pastelBlue snap-start">
+                <h1>Second Page</h1>
+            </section>
+
+            <section className="h-screen flex bg-pastelPurple snap-start">
+                <h1>Third Page</h1>
+            </section>
+
+            <section className="h-screen flex bg-pastelYellow snap-start">
+                <h1>Fourth Page</h1>
+            </section>
+            
+
+        </div>
+        </div>
+    )
+}
