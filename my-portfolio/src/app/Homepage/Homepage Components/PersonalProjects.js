b641e36dff36cfb6fc22eb63d5cbed3c6d1887e3
Micah Shumaker <micah.shumaker@gmail.com>
2024-01-09 22:32:27 -0700

    Personal project commit

--- my-portfolio/src/app/Homepage/Homepage Components/PersonalProjects.js
+++ my-portfolio/src/app/Homepage/Homepage Components/PersonalProjects.js
@@ -6,7 +6,7 @@ export default function PersonalProject() {
         <section className="h-[300-vh]  bg-jetBlack snap-start">
             <div className="grid grid-col-3 grid-rows-2 pb-10 pt-40 ms px-28 gap-10 col-auto">
 
-                <div className="col-start-1 row-start-2 border border-limeGreen bg-limeGreen h-96 rounded-3xl  transition-all duration-200 ease-linear">
+                <div className="col-start-1 row-start-2 border border-limeGreen bg-secondary h-96 rounded-3xl  transition-all duration-200 ease-linear">
                     <h2 className="text-center text-jetBlack font-headerFont text-6xl p-5 ">perspective.io</h2>
                     <div className="grid grid-col-2 grid-row-1">
                         <div className="col-start-1">   
@@ -19,7 +19,7 @@ export default function PersonalProject() {
                 <div className="col-start-2 row-start-1 row-span-2 border border-secondary bg-secondary  w-40 rounded-3xl  transition-all duration-200 ease-linear"></div>
                 <div className="col-start-3 row-start-1 row-span-2 border border-skyBlue bg-skyBlue   w-40 rounded-3xl transition-all duration-200 ease-linear"></div>
                 <div className="z-20  col-start-2 col-span-2 h-48 row-start-1 border border-secondary bg-secondary rounded-3xl flex justify-center items-center"><h2 className="text-jetBlack font-headerFont text-5xl">Personal Projects</h2></div>
-                <div className="col-start-1 row-start-1 border border-poppyRed bg-poppyRed h-96 rounded-3xl  transition-all duration-200 ease-linear">
+                <div className="col-start-1 row-start-1 border border-poppyRed bg-secondary h-96 rounded-3xl  transition-all duration-200 ease-linear">
                     <h2 className="text-center font-headerFont text-6xl p-5 text-jetBlack ">micahshu.dev</h2>
                     <h3 className="text-left p-8 font-bodyFont text-2xl text-jetBlack ">Technology: Next.Js | TailWind</h3>
                     <p className="text-left p-8 font-bodyFont text-2xl text-jetBlack ">I created this site to be a hub for my digital presence. My first dive into designing with Tailwind.</p>
